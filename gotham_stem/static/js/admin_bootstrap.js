b7c31a23b470ee333c70ada9862883c2c4762864
Neelay Shah <nil12285@gmail.com>
2025-12-18 17:57:49 +0530

    init project

---
+++ gotham_stem/static/js/admin_bootstrap.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.body?.id !== "wagtail") return;
+
+  document.querySelectorAll(
+    'form input:not([type="checkbox"]):not([type="radio"]):not([type="file"]):not([type="submit"]), ' +
+    'form textarea'
+  ).forEach(function (el) {
+    el.classList.add("form-control");
+  });
+
+  document.querySelectorAll('form select').forEach(function (el) {
+    el.classList.add("form-select");
+  });
+});
+
+
